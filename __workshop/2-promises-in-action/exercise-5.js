33487a608f6ce69133e3f3b1652402410ec07b25
Kendra Scappaticci <k.scapp@gmail.com>
2020-04-21 21:49:10 -0400

    all done module-3-7

--- __workshop/2-promises-in-action/exercise-5.js
+++ __workshop/2-promises-in-action/exercise-5.js
@@ -1,5 +1,8 @@
 const request = require('request-promise');
 
 const getGeekJoke = async () => {
-
+    const fuckOff = await request('https://geek-jokes.sameerkumar.website/api');
+    return (fuckOff);
 }
+
+module.exports = { getGeekJoke };
--- server.js
+++ server.js
@@ -4,20 +4,40 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
+const { getDadJoke } = require('./__workshop/2-promises-in-action/exercise-3');
+const { getTronaldDump } = require('./__workshop/2-promises-in-action/exercise-4');
+const { getGeekJoke } = require('./__workshop/2-promises-in-action/exercise-5');
+
 const PORT = process.env.PORT || 8000;
 
+const handleJoke = async (req, res) => {
+    const type = req.params.type;
+    if (type === 'dad') {
+        const joke = await getDadJoke();
+        res.send({ joke });
+    } else if (type === 'geek') {
+        const joke = await getTronaldDump();
+        console.log(joke);
+        res.send({ joke });
+    } else {
+        const joke = await getGeekJoke();
+        console.log(joke);
+        res.send({ joke });
+    }
+};
+
 express()
-    .use(function(req, res, next) {
+    .use(function (req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         next();
     })
-	.use(morgan('dev'))
-	.use(express.static('public'))
+    .use(morgan('dev'))
+    .use(express.static('public'))
     .use(bodyParser.json())
-    .use(express.urlencoded({extended: false}))
+    .use(express.urlencoded({ extended: false }))
     .set('view engine', 'ejs')
 
     // endpoints
-
+    .get('/gimme-joke/:type', handleJoke)
     .listen(PORT, () => console.log(`Listening on port ${PORT}`));
